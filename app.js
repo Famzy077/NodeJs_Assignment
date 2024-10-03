2b463abfe0bdcf67a52e77bd002fb9b99272180b
Akinola Femi <149377080+Famzy077@users.noreply.github.com>
2024-10-03 09:41:01 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-})
-app.listen(4000, () => {
-    console.log('App running on PORT:4000');
-});
